0698605e796f8810911a3f5ed1975f8bad609d3e
hanupriya <hanupriya28@gmail.com>
2018-04-21 22:51:21 +0530

    Comment model is added

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from .models import Comment
 from .forms import Postform
+from .forms import Commentform
 from django.shortcuts import redirect
 from django.utils import timezone
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+
 # Create your views here.
 
 
 def post_list(request):
-    posts = Post.objects.all() #filter(title__contains='Energy').order_by('published_date') #created a QuerySet
+    posts = Post.objects.all()
+    #filter(title__contains='Energy').order_by('published_date') #created a QuerySet
     return render(request, 'blog/post_list.html',{'posts':posts}) #request: anything from user / blog/post_list.html: template page /posts : to fill in the template with data
 
 def post_detail(request,pk):
@@ -16,13 +22,14 @@ def post_detail(request,pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post':post})
 
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = Postform(request.POST)
         if(form.is_valid()):
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            # post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -30,6 +37,7 @@ def post_new(request):
     return render(request, 'blog/post_edit.html',{'form':form})
 
 
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -37,9 +45,60 @@ def post_edit(request, pk):
          if(form.is_valid()):
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            # post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = Postform(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+@login_required
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts':posts})
+
+@login_required
+def post_publish(request,pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=post.pk)
+
+@login_required
+def post_remove(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    post.delete()
+    return redirect(post_list)
+
+def logout_view(request):
+    logout(request)
+    return redirect(logout_view)
+
+def add_comment(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = Commentform(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = Commentform()
+    return render(request, 'blog/add_comment.html', {'form': form})
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.delete()
+    return redirect('post_detail', pk=comment.post.pk)
+
+
+
+
+
